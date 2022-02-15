3226f798524a33695ed3f143d5879b656cfde00d
Asim Saeed <zedge744@live.com>
2022-02-15 16:35:27 +0500

    updated sales model
    added salesmodel

--- src/sales/admin.py
+++ src/sales/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+#Model defined to represent admin view
+admin.site.register(Position)
+admin.site.register(Sale)
+admin.site.register(CSV)
+
+
